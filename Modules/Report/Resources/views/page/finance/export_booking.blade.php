76f96dc31ed2758ed9f0e4fce6c765141610ad0c
itoktoni <itok.toni@gmail.com>
2023-04-11 13:14:46 +0700

    update

--- Modules/Report/Resources/views/page/finance/export_booking.blade.php
+++ Modules/Report/Resources/views/page/finance/export_booking.blade.php
@@ -28,15 +28,20 @@
                 <th class="text-right" style="width:10%">DP</th>
                 <th class="text-right" style="width:10%">Discount</th>
                 <th class="text-right" style="width:10%">Total</th>
-                <th class="text-right" style="width:15%">Total + PPN</th>
+                <th class="text-right" style="width:15%">Tagihan</th>
                 <th class="text-right" style="width:15%">Metode</th>
                 <th class="text-right" style="width:10%">Status</th>
             </tr>
         </thead>
         <tbody>
-
+            @php
+            $grand_total = 0;
+            @endphp
             @foreach($preview as $data)
-
+            @php
+            $total = (($data->booking_value + $data->booking_dp) - $data->booking_discount_value);
+            $grand_total = $grand_total + $total;
+            @endphp
             <tr>
                 <td data-title="No">{{ $loop->iteration }} </td>
                 <td data-title="No. Order">{{ $data->booking_code ?? '' }} </td>
@@ -48,7 +53,7 @@
                 <td class="text-right" data-title="Total">{{ Helper::createRupiah($data->booking_value) }} </td>
                 <td class="text-right" data-title="Total">{{ Helper::createRupiah($data->booking_dp) }} </td>
                 <td class="text-right" data-title="Total">{{ Helper::createRupiah($data->booking_discount_value) }}</td>
-                <td class="text-right" data-title="Pembayaran">{{ Helper::createRupiah(($data->booking_value + $data->booking_dp) - $data->booking_discount_value) }} </td>
+                <td class="text-right" data-title="Pembayaran">{{ Helper::createRupiah($total) }} </td>
                 <td class="text-right" data-title="Pembayaran">{{ Helper::createRupiah($data->booking_summary) }} </td>
                 <td class="text-right" data-title="Outstanding">{{ $data->booking_metode }} </td>
                 <td class="text-right" data-title="Outstanding">{{ BookingType::getDescription($data->booking_status) }} </td>
@@ -64,7 +69,8 @@
         <tr>
             <td class="total" data-title="" colspan="13">Grand Total</td>
             <td class="total text-right" data-title="Grand Total">
-                {{ Helper::createRupiah($preview->sum('booking_summary')) }}</td>
+                {{ Helper::createRupiah($grand_total) }}
+            </td>
         </tr>
     </table>
 
